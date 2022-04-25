d67154ebe72205c6063c4bdd00540c9d3a2e9126
Nikita Ihnatenko <nikitaihnatenko@MacBook-Pro-Nikita.local>
2022-04-25 20:19:58 +0300

    add modal window nav

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,38 @@
 const nav__linkm = document.getElementById("nav__item-mens")
 const menu__blockm = document.getElementById("item__block-mens");
 
+let check = 0;
+
 nav__linkm.addEventListener('click', function onClick() {
-    menu__blockm.style.display = 'block';
+    menu__blockm.style.display = 'flex';
+    check = check + 1;
 });
 
-
-document.addEventListener ( "click", function(event) {
-  if (event.target == menu__blockm) {
+window.addEventListener ( "click", function(event) {
+  const isDescendant = function (parent, child) {
+    let node = child;
+    while (node) {
+        if (node === parent) {
+            return true;
+        }
+  
+        // Traverse up to the parent
+        node = node.parentNode;
+    }
+    // Go up until the root but couldn't find the parent
+    return false;
+  };
+  console.log(event.target);
+  console.log(menu__blockm.children);
+  if (isDescendant(menu__blockm,event.target)) return;
+  if (event.target !== nav__linkm) {
     menu__blockm.style.display = "none";
+  }else
+  {
+    if( check %2 === 0)
+    {
+      menu__blockm.style.display = "none";
+    }
   }
 });
 
