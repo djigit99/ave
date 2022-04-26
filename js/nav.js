bd8a9b8a2b60f7ca1c029f77dcaef24f428026e0
Nikita Ihnatenko <nikitaihnatenko@MacBook-Pro-Nikita.local>
2022-04-26 13:39:30 +0300

    create select currency

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,11 @@
 const nav__linkm = document.getElementById("nav__item-mens")
 const menu__blockm = document.getElementById("item__block-mens");
 
-let check = 0;
+let check1 = 0;
 
 nav__linkm.addEventListener('click', function onClick() {
     menu__blockm.style.display = 'flex';
-    check = check + 1;
+    check1 = check1+ 1;
 });
 
 window.addEventListener ( "click", function(event) {
@@ -22,14 +22,13 @@ window.addEventListener ( "click", function(event) {
     // Go up until the root but couldn't find the parent
     return false;
   };
-  console.log(event.target);
-  console.log(menu__blockm.children);
+
   if (isDescendant(menu__blockm,event.target)) return;
   if (event.target !== nav__linkm) {
     menu__blockm.style.display = "none";
   }else
   {
-    if( check %2 === 0)
+    if( check1 %2 === 0)
     {
       menu__blockm.style.display = "none";
     }
@@ -39,20 +38,111 @@ window.addEventListener ( "click", function(event) {
 const nav__linkw = document.getElementById("nav__item-womens")
 const menu__blockw = document.getElementById("item__block-womens");
 
-nav__linkw.addEventListener('click', function () {
-    menu__blockw.classList.toggle('item__block-active');
+let check2 = 0;
+
+nav__linkw.addEventListener('click', function onClick() {
+    menu__blockw.style.display = 'flex';
+    check2 = check2 + 1;
+});
+
+window.addEventListener ( "click", function(event) {
+  const isDescendant = function (parent, child) {
+    let node = child;
+    while (node) {
+        if (node === parent) {
+            return true;
+        }
+  
+        // Traverse up to the parent
+        node = node.parentNode;
+    }
+    // Go up until the root but couldn't find the parent
+    return false;
+  };
+
+  if (isDescendant(menu__blockw,event.target)) return;
+  if (event.target !== nav__linkw) {
+    menu__blockw.style.display = "none";
+  }else
+  {
+    if( check2 %2 === 0)
+    {
+      menu__blockw.style.display = "none";
+    }
+  }
 });
 
 const nav__linklc = document.getElementById("nav__item-lc")
 const menu__blocklc = document.getElementById("item__block-lc");
 
-nav__linklc.addEventListener('click', function () {
-    menu__blocklc.classList.toggle('item__block-active');
+let check3 = 0;
+
+nav__linklc.addEventListener('click', function onClick() {
+    menu__blocklc.style.display = 'flex';
+    check3 = check3 + 1;
+});
+
+window.addEventListener ( "click", function(event) {
+  const isDescendant = function (parent, child) {
+    let node = child;
+    while (node) {
+        if (node === parent) {
+            return true;
+        }
+  
+        // Traverse up to the parent
+        node = node.parentNode;
+    }
+    // Go up until the root but couldn't find the parent
+    return false;
+  };
+
+  if (isDescendant(menu__blocklc,event.target)) return;
+  if (event.target !== nav__linklc) {
+    menu__blocklc.style.display = "none";
+  }else
+  {
+    if( check3 %2 === 0)
+    {
+      console.log(check3);
+      menu__blocklc.style.display = "none";
+    }
+  }
 });
 
 const nav__linkbk = document.getElementById("nav__item-bk")
 const menu__blockbk = document.getElementById("item__block-bk");
 
-nav__linkbk.addEventListener('click', function () {
-    menu__blockbk.classList.toggle('item__block-active');
+let check4 = 0;
+
+nav__linkbk.addEventListener('click', function onClick() {
+    menu__blockbk.style.display = 'flex';
+    check4 = check4 + 1;
+});
+
+window.addEventListener ( "click", function(event) {
+  const isDescendant = function (parent, child) {
+    let node = child;
+    while (node) {
+        if (node === parent) {
+            return true;
+        }
+  
+        // Traverse up to the parent
+        node = node.parentNode;
+    }
+    // Go up until the root but couldn't find the parent
+    return false;
+  };
+
+  if (isDescendant(menu__blockbk,event.target)) return;
+  if (event.target !== nav__linkbk) {
+    menu__blockbk.style.display = "none";
+  }else
+  {
+    if( check4 %2 === 0)
+    {
+      menu__blockbk.style.display = "none";
+    }
+  }
 });
